95503db1a656f6cb522fd21857ab89a1623056b8
Giovanni Petrache <giovanni.petrache@codingavenue.com>
2017-03-26 10:46:27 -0700

    Added build to phar file

--- console.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once "vendor/autoload.php";
-
-use Symfony\Component\Console\Application;
-use Symfony\Component\Console\Input\ArgvInput;
-
-$app = new Application('Illuminate Education Tools', 'v0.1.0');
-
-$app->run(new ArgvInput());
