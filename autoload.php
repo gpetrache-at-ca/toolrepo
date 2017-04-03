0829061b9d7e9ce7304b76b46cec6a1bec1ff92e
Giovanni Petrache <giovanni.petrache@codingavenue.com>
2017-04-03 22:01:24 +0800

    Updates?

--- autoload.php
+++ autoload.php
@@ -1,11 +1,17 @@
 <?php
 
-use Composer\Autoload\ClassLoader;
-
-/**
- * @var ClassLoader $loader
- */
-$loader = require __DIR__.'/./vendor/autoload.php';
+foreach ([__DIR__ . '/../../autoload.php', __DIR__ . '/vendor/autoload.php'] as $file) {
+    if (file_exists($file) && !defined('TOOLREPO_COMPOSER_INSTALL')) {
+        define('TOOLREPO_COMPOSER_INSTALL', $file);
+        break;
+    }
+}
+unset($file);
+if (! defined('TOOLREPO_COMPOSER_INSTALL')) {
+    fwrite(STDERR, 'You need to run "composer.phar install" to install ToolRepo.' . PHP_EOL);
+    exit(1);
+} 
+$loader = require_once TOOLREPO_COMPOSER_INSTALL;
 
 return $loader;
 
